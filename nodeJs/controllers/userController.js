bdd7ca47d5ab3e1507b665c3f7bfb33da2dcbf95
arvinder singh <arvindersingh6961@gmail.com>
2024-05-15 13:21:18 -0400

    Login api create

--- nodeJs/controllers/userController.js
+++ nodeJs/controllers/userController.js
@@ -2,6 +2,8 @@
 const User = require("../models/userModel")
 const bcrypt = require("bcrypt")
 const saltround = 10
+const jwt=require("jsonwebtoken")
+const secretKey="0702"
 
 exports.register = (req, res) => {
     let validation = ""
@@ -30,7 +32,7 @@ exports.register = (req, res) => {
                     userobj.email = req.body.email
                     userobj.password = bcrypt.hashSync(req.body.password, saltround)
                     userobj.save()
-                        .then(userdata => {
+                        .then(userData => {
                             res.json({
                                 status: 200,
                                 success: true,
@@ -60,3 +62,73 @@ exports.register = (req, res) => {
     }
 
 }
+
+exports.login = (req, res) => {
+    let validation = "";
+    if (req.body.email === "") {
+        validation += "Enter email. ";
+    }
+    if (req.body.password === "") {
+        validation += "Enter password. ";
+    }
+    if (!!validation) {
+        return res.status(400).json({
+            status: 400,
+            success: false,
+            msg: validation.trim()
+        });
+    } else {
+        User.findOne({ email: req.body.email })
+            .then(userData => {
+                if (!userData) {
+                    return res.status(400).json({
+                        status: 400,
+                        success: false,
+                        msg: "User does not exist"
+                    });
+                } else {
+                    bcrypt.compare(req.body.password, userData.password)
+                        .then(passwordMatch => {
+                            if (!passwordMatch) {
+                                return res.status(400).json({
+                                    status: 400,
+                                    success: false,
+                                    msg: "Invalid password"
+                                });
+                            } else {
+                                const payload = {
+                                    name: userData.name,
+                                    email: userData.email
+                                };
+                                const token = jwt.sign(payload, secretKey, {
+                                    expiresIn: '11h' 
+                                });
+                                return res.status(200).json({
+                                    status: 200,
+                                    success: true,
+                                    msg: "Login Successful",
+                                    data: userData,
+                                    token: token
+                                });
+                            }
+                        })
+                        .catch(err => {
+                            console.error("Error comparing passwords:", err);
+                            return res.status(500).json({
+                                status: 500,
+                                success: false,
+                                msg: "Internal server error"
+                            });
+                        });
+                }
+            })
+            .catch(err => {
+                console.error("Error finding user:", err);
+                return res.status(500).json({
+                    status: 500,
+                    success: false,
+                    msg: "Internal server error"
+                });
+            });
+    }
+};
--- nodeJs/routes/route.js
+++ nodeJs/routes/route.js
@@ -3,6 +3,7 @@ const Route=require("express").Router()
 const userController=require("../controllers/userController")
 
 Route.post("/register",userController.register)
+Route.post("/login",userController.login)
 
 
 
